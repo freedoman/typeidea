b5c6cabc83afea19790b7de6d7670df5b3171774
freedoman@aliyun <lizhan20161105@gmail.com>
2020-05-04 22:45:30 +0800

    change django version requirement & add allow host

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@!o$_%znak!(m=+a2*_mrr5@&ka@g(6^yoiq6w0fhc1_ap0j=i'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
